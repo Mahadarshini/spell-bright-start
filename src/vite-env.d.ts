44648717008c6920bb8aadae74e15b597744a933
bhuvaneshwarikanagaraj <bhuvaneshwarii026@gmail.com>
2025-08-22 11:26:55 +0530

    feat:dashboard

--- src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-// Local ambient declarations to avoid requiring 'vite/client' types
-declare interface ImportMetaEnv {
-  readonly [key: string]: string;
-}
-
-declare interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
